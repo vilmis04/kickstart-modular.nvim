fcef663e9a9fc0704bc340d9edc0947f0f84dbd7
Vilmantas Sudaris <vilmantas.sudaris@seb.se>
2025-06-05 14:46:59 +0300

    feat: add copilot

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -55,6 +55,8 @@ require('lazy').setup({
   require 'kickstart/plugins/git-conflict',
   -- require 'kickstart/plugins/diffview',
 
+  require 'kickstart/plugins/copilot',
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -66,7 +68,7 @@ require('lazy').setup({
   --
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
 
